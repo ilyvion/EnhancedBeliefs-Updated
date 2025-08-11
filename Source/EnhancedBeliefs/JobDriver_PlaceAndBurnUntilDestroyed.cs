42452063c0c26c97d4bb66cd5733efc1bdaa6c8a
Alexander Krivács Schrøder <alexschrod@gmail.com>
2025-08-11 09:02:29 +0200

    chore: add workspace configuration for EnhancedBeliefs project

--- Source/EnhancedBeliefs/JobDriver_PlaceAndBurnUntilDestroyed.cs
+++ Source/EnhancedBeliefs/JobDriver_PlaceAndBurnUntilDestroyed.cs
@@ -40,7 +40,7 @@ internal sealed class JobDriver_PlaceAndBurnUntilDestroyed : JobDriver
         yield return Toils_General.Wait(90);
         yield return Toils_Jump.JumpIf(tryIgniteAgain, () => !TargetThingA.IsBurning());
         yield return Toil_EnhancedBeliefs.BurnBook().FailOn(() => !TargetThingA.IsBurning());
-        yield return Toils_Jump.JumpIf(tryIgniteAgain, () => !TargetThingA.IsBurning());
+        yield return Toils_Jump.JumpIf(tryIgniteAgain, () => !TargetThingA.IsBurning() || !TargetThingA.Destroyed);
     }
 
     private static Toil TryIgniteAgain()
@@ -54,72 +54,75 @@ internal sealed class Toil_EnhancedBeliefs
     public static Toil BurnBook()
     {
         var toil = ToilMaker.MakeToil("BurnBook");
-        toil.initAction = delegate
-        {
-            var actor = toil.actor;
-            var target = actor.jobs.curJob.GetTarget(TargetIndex.A);
-
-            actor.rotationTracker.FaceCell(target.Cell);
-            actor.jobs.curDriver.rotateToFace = TargetIndex.A;
-        };
-        toil.tickAction = delegate
-        {
-            var actor = toil.actor;
-            var book = (Thing)actor.jobs.curJob.GetTarget(TargetIndex.A);
-
-            if (!book.Destroyed)
-            {
-                _ = book.TakeDamage(new DamageInfo(DamageDefOf.Burn, 0.2f, 100f, instigator: actor));
-            }
-        };
-        toil.AddFinishAction(delegate
-        {
-            var actor = toil.actor;
-            var book = (ThingWithComps)actor.jobs.curJob.GetTarget(TargetIndex.A);
-
-            if (book == null || !book.Destroyed)
-            {
-                return;
-            }
-
-            var comp = book.GetComp<CompBook>();
-            Ideo? ideo = null;
-
-            foreach (var doer in comp.doers)
-            {
-                if (doer is ReadingOutcomeDoer_CertaintyChange change)
-                {
-                    ideo = change.ideo;
-                    break;
-                }
-            }
-
-            if (ideo != null)
-            {
-                Messages.Message(
-                    "EnhancedBeliefs.BookBurningSuccess".Translate(
-                        actor.Named("PAWN"), book.Named("BOOK"), ideo.Named("IDEO")),
-                    actor,
-                    Find.FactionManager.OfPlayer.ideos.PrimaryIdeo == ideo
-                        ? MessageTypeDefOf.NegativeEvent
-                        : MessageTypeDefOf.NeutralEvent);
-            }
-        });
-        toil.AddEndCondition(delegate
-        {
-            var actor = toil.actor;
-            var book = (Thing)actor.jobs.curJob.GetTarget(TargetIndex.A);
-            return (book?.Destroyed ?? true) ? JobCondition.Succeeded : JobCondition.Ongoing;
-        });
+        toil.initAction = () => BurnBook_InitAction(toil);
+        toil.tickAction = () => BurnBook_TickAction(toil);
+        toil.AddFinishAction(() => BurnBook_FinishAction(toil));
+        toil.AddEndCondition(() => BurnBook_EndCondition(toil));
         toil.defaultCompleteMode = ToilCompleteMode.Never;
         toil.handlingFacing = true;
-        _ = toil.WithProgressBar(TargetIndex.A, () =>
-        {
-            var actor = toil.actor;
-            var book = (Thing)actor.jobs.curJob.GetTarget(TargetIndex.A);
-            return 1f - (book.HitPoints / ((float)book.MaxHitPoints));
-        });
-        //toil.FailOnCannotTouch(TargetIndex.A, PathEndMode.InteractionCell);
+        _ = toil.WithProgressBar(TargetIndex.A, () => BurnBook_ProgressBarGetter(toil));
         return toil;
+    }
+
+    private static void BurnBook_InitAction(Toil toil)
+    {
+        var actor = toil.actor;
+        var target = actor.jobs.curJob.GetTarget(TargetIndex.A);
+        actor.rotationTracker.FaceCell(target.Cell);
+        actor.jobs.curDriver.rotateToFace = TargetIndex.A;
+    }
+
+    private static void BurnBook_TickAction(Toil toil)
+    {
+        var actor = toil.actor;
+        var book = (Thing)actor.jobs.curJob.GetTarget(TargetIndex.A);
+        if (!book.Destroyed)
+        {
+            _ = book.TakeDamage(new DamageInfo(DamageDefOf.Burn, 0.2f, 100f, instigator: actor));
+        }
+    }
+
+    private static void BurnBook_FinishAction(Toil toil)
+    {
+        var actor = toil.actor;
+        var book = (ThingWithComps)actor.jobs.curJob.GetTarget(TargetIndex.A);
+        if (book == null || !book.Destroyed)
+        {
+            return;
+        }
+        var comp = book.GetComp<CompBook>();
+        Ideo? ideo = null;
+        foreach (var doer in comp.doers)
+        {
+            if (doer is ReadingOutcomeDoer_CertaintyChange change)
+            {
+                ideo = change.ideo;
+                break;
+            }
+        }
+        if (ideo != null)
+        {
+            Messages.Message(
+                "EnhancedBeliefs.BookBurningSuccess".Translate(
+                    actor.Named("PAWN"), book.Named("BOOK"), ideo.Named("IDEO")),
+                actor,
+                Find.FactionManager.OfPlayer.ideos.PrimaryIdeo == ideo
+                    ? MessageTypeDefOf.NegativeEvent
+                    : MessageTypeDefOf.NeutralEvent);
+        }
+    }
+
+    private static JobCondition BurnBook_EndCondition(Toil toil)
+    {
+        var actor = toil.actor;
+        var book = (Thing)actor.jobs.curJob.GetTarget(TargetIndex.A);
+        return (book?.Destroyed ?? true) ? JobCondition.Succeeded : JobCondition.Ongoing;
+    }
+
+    private static float BurnBook_ProgressBarGetter(Toil toil)
+    {
+        var actor = toil.actor;
+        var book = (Thing)actor.jobs.curJob.GetTarget(TargetIndex.A);
+        return 1f - (book.HitPoints / ((float)book.MaxHitPoints));
     }
 }
--- Source/EnhancedBeliefs/MentalStateWorker_Iconoclast.cs
+++ Source/EnhancedBeliefs/MentalStateWorker_Iconoclast.cs
@@ -7,7 +7,11 @@ internal sealed class MentalStateWorker_Iconoclast : MentalStateWorker
     public override bool StateCanOccur(Pawn pawn)
     {
         return base.StateCanOccur(pawn) &&
+#if !v1_5
             GenClosest.ClosestThing_Global_Reachable(
+#else
+            GenClosest.ClosestThing_Global_Reachable_NewTemp(
+#endif
                 pawn.Position,
                 pawn.Map,
                 pawn.Map.listerThings.AllThings,
